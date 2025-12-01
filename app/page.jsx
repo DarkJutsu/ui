9a6f086e639953cf189a4abb32599c424ec9185d
DarkJutsu <ulloajose34@gmail.com>
2025-11-30 20:03:46 -0600

    feat(app):added like-button.jsx & refactor page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,3 @@
-import {useState} from "react";
-
 function Header({title}) {
     console.log(title)
     return <h1>Develop {title ? title : 'JavaScript'}. Preview. Ship. 🚀</h1>
@@ -7,12 +5,6 @@ function Header({title}) {
 
 export function HomePage() {
     const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton']
-    const [likes, setLikes] = useState(0)
-
-    function handleClick() {
-        setLikes(likes + 1)
-    }
-
     return (<div>
         <Header title="React"/>
         <ul>
@@ -20,6 +12,5 @@ export function HomePage() {
                 <li key={name}>{name}</li>
             ))}
         </ul>
-        <button onClick={handleClick}>Like({likes})</button>
     </div>)
 }
